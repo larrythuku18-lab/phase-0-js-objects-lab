edd42c780d98cf4c6416b8fa06bf89346e6f96b5
Larry Thuku <larrythuku18@gmail.com>
2026-03-29 14:40:51 +0300

    First javascript assignment

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 //Write your code here
+// Declare and intialize an Attendee object.
+const attendee = {
+  attendeeId : "T001",
+  name : "Alice smith ",
+  event : "JavaScript Conference",
+  ticketType : "VIP",
+  ticketPrice : 150.00
 
-
+};
+ 
+function logAttendeeName(attendee) {
+  console.log(attendee.name);
+};
+function logTicketPrice(attendee) {
+  console.log(attendee.ticketPrice);
+}
+function updateTicketType(attendee, newTicketType) {
+  attendee.ticketType = newTicketType;
+}
+function updateTicketPrice(attendee, newTicketPrice)  {
+  attendee.ticketPrice = newTicketPrice;
+}
+function removeEventProperty(attendee) {
+  delete attendee.event;
+}
+function addCheckedInProperty(attendee) {
+  attendee.checkedIn = true;
+}
 
 //Needed for the tests to work. Don't modify
 module.exports = {
